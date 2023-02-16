80ecf1f279df66e62411ce457dd91049c71dc2c7
mac os <rkolchev65@gmail.com>
2023-02-16 17:38:43 +0200

    22. Withdraws

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -44,7 +44,7 @@ const Balance = () => {
       setToken1TransferAmount(e.target.value)
     } else {
       setToken2TransferAmount(e.target.value)
-    } console.log({ token2TransferAmount })
+    }
   }
 
 
@@ -57,6 +57,19 @@ const Balance = () => {
       setToken1TransferAmount(0)
     } else {
       transferTokens(provider, exchange, 'Deposit', token, token2TransferAmount, dispatch)
+      setToken2TransferAmount(0)
+    }
+    
+  }
+
+  const withdrawHandler = (e, token) => {
+    e.preventDefault()
+
+    if (token.address === tokens[0].address) {
+      transferTokens(provider, exchange, 'Withdraw', token, token1TransferAmount, dispatch)
+      setToken1TransferAmount(0)
+    } else {
+      transferTokens(provider, exchange, 'Withdraw', token, token2TransferAmount, dispatch)
       setToken2TransferAmount(0)
     }
     
@@ -87,7 +100,7 @@ const Balance = () => {
           <p><small>Exchange</small><br />{exchangeBalances && exchangeBalances[0]}</p>
         </div>
 
-        <form onSubmit={(e) => depositHandler(e, tokens[0])}>
+        <form onSubmit={isDeposit ? (e) => depositHandler(e, tokens[0]) : (e) => withdrawHandler(e, tokens[0])}>
           <label htmlFor="token0">{symbols && symbols[0]} Amount</label>
           <input 
             type="text" 
@@ -117,7 +130,7 @@ const Balance = () => {
           <p><small>Exchange</small><br />{exchangeBalances && exchangeBalances[1]}</p>
         </div>
 
-        <form onSubmit={(e) => depositHandler(e, tokens[1])}>
+        <form onSubmit={isDeposit ? (e) => depositHandler(e, tokens[1]) : (e) => withdrawHandler(e, tokens[1])}>
           <label htmlFor="token1"></label>
           <input 
             type="text" 
